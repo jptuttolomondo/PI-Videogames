4989d359edab85479662f6666a1ac4f0b53049e9
Juan Pablo Tuttolomondo <jptuttolomondo@hotmail.com>
2023-01-09 21:05:01 -0300

    redeploy vercel desde railway

--- client/assets/constants.js
+++ client/assets/constants.js
@@ -1,5 +1,5 @@
-const baseUrl = "https://videogames-api-lm.herokuapp.com"
-
+//const baseUrl = "https://videogames-api-lm.herokuapp.com"
+const baseUrl = "https://pi-videogames-production-f9c4.up.railway.app/"
 export const URL_GET_ALL_VIDEOGAMES = `${baseUrl}/videogames`
 export const URL_GET_GENRES = `${baseUrl}/genres`
 export const URL_SEARCH_BY_NAME = `${baseUrl}/videogames?name=`
